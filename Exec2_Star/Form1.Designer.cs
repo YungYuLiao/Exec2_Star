4211dfd2baf94b78349356dcc6124cd5fc8cc9c0
User <User@DESKTOP-QVNS494>
2022-10-24 19:53:04 +0800

    加入專案檔案。

---
+++ Exec2_Star/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Exec2_Star
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lblRows = new System.Windows.Forms.Label();
+			this.btmLeft = new System.Windows.Forms.Button();
+			this.txtRows = new System.Windows.Forms.TextBox();
+			this.txtResult = new System.Windows.Forms.TextBox();
+			this.btmMiddle = new System.Windows.Forms.Button();
+			this.btmRight = new System.Windows.Forms.Button();
+			this.lblInfo = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// lblRows
+			// 
+			this.lblRows.AutoSize = true;
+			this.lblRows.Font = new System.Drawing.Font("微軟正黑體", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.lblRows.Location = new System.Drawing.Point(20, 46);
+			this.lblRows.Name = "lblRows";
+			this.lblRows.Size = new System.Drawing.Size(43, 18);
+			this.lblRows.TabIndex = 0;
+			this.lblRows.Text = "列數 :";
+			// 
+			// btmLeft
+			// 
+			this.btmLeft.Font = new System.Drawing.Font("微軟正黑體", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.btmLeft.Location = new System.Drawing.Point(246, 44);
+			this.btmLeft.Name = "btmLeft";
+			this.btmLeft.Size = new System.Drawing.Size(75, 32);
+			this.btmLeft.TabIndex = 1;
+			this.btmLeft.Text = "靠左對齊";
+			this.btmLeft.UseVisualStyleBackColor = true;
+			this.btmLeft.Click += new System.EventHandler(this.btmLeft_Click);
+			// 
+			// txtRows
+			// 
+			this.txtRows.Location = new System.Drawing.Point(66, 44);
+			this.txtRows.Name = "txtRows";
+			this.txtRows.Size = new System.Drawing.Size(157, 22);
+			this.txtRows.TabIndex = 2;
+			// 
+			// txtResult
+			// 
+			this.txtResult.Location = new System.Drawing.Point(24, 81);
+			this.txtResult.MaxLength = 2;
+			this.txtResult.Multiline = true;
+			this.txtResult.Name = "txtResult";
+			this.txtResult.Size = new System.Drawing.Size(199, 117);
+			this.txtResult.TabIndex = 3;
+			this.txtResult.WordWrap = false;
+			// 
+			// btmMiddle
+			// 
+			this.btmMiddle.Font = new System.Drawing.Font("微軟正黑體", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.btmMiddle.Location = new System.Drawing.Point(246, 105);
+			this.btmMiddle.Name = "btmMiddle";
+			this.btmMiddle.Size = new System.Drawing.Size(75, 32);
+			this.btmMiddle.TabIndex = 4;
+			this.btmMiddle.Text = "等腰";
+			this.btmMiddle.UseVisualStyleBackColor = true;
+			this.btmMiddle.Click += new System.EventHandler(this.btmMiddle_Click);
+			// 
+			// btmRight
+			// 
+			this.btmRight.Font = new System.Drawing.Font("微軟正黑體", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.btmRight.Location = new System.Drawing.Point(246, 166);
+			this.btmRight.Name = "btmRight";
+			this.btmRight.Size = new System.Drawing.Size(75, 32);
+			this.btmRight.TabIndex = 5;
+			this.btmRight.Text = "靠右對齊";
+			this.btmRight.UseVisualStyleBackColor = true;
+			this.btmRight.Click += new System.EventHandler(this.btmRight_Click);
+			// 
+			// lblInfo
+			// 
+			this.lblInfo.AutoSize = true;
+			this.lblInfo.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.lblInfo.Location = new System.Drawing.Point(21, 15);
+			this.lblInfo.Name = "lblInfo";
+			this.lblInfo.Size = new System.Drawing.Size(208, 17);
+			this.lblInfo.TabIndex = 6;
+			this.lblInfo.Text = "輸入列數(1~10)，並選擇排列方式";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(343, 219);
+			this.Controls.Add(this.lblInfo);
+			this.Controls.Add(this.btmRight);
+			this.Controls.Add(this.btmMiddle);
+			this.Controls.Add(this.txtResult);
+			this.Controls.Add(this.txtRows);
+			this.Controls.Add(this.btmLeft);
+			this.Controls.Add(this.lblRows);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "星星塔生成器";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label lblRows;
+		private System.Windows.Forms.Button btmLeft;
+		private System.Windows.Forms.TextBox txtRows;
+		private System.Windows.Forms.TextBox txtResult;
+		private System.Windows.Forms.Button btmMiddle;
+		private System.Windows.Forms.Button btmRight;
+		private System.Windows.Forms.Label lblInfo;
+	}
+}
+
---
+++ Exec2_Star/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exec2_Star
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btmLeft_Click(object sender, EventArgs e)
+		{
+			//取得列數
+			int row = 0;
+			try
+			{
+				row = GetRows();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+			//生成靠左星號
+			string stars = GenerateLeftStars(row);
+			//呈現星號
+			Display(stars);
+		}
+
+		private void Display(string stars)
+		{
+			txtResult.Text = stars;
+		}
+
+		private string GenerateLeftStars(int row)
+		{
+			string stars = string.Empty;
+			for (int i = 1; i <= row; i++)
+			{
+				stars += new string('*', i) + "\r\n";
+			}
+			return stars;
+		}
+
+		private int GetRows()
+		{
+			string input = txtRows.Text;
+			bool isInt = int.TryParse(input, out int row);
+			if (isInt == false)
+			{
+				throw new Exception("請輸入列數");
+			}
+			if(row <= 0)
+			{
+				throw new Exception("列數必須大於0");
+			}
+			if(row > 10)
+			{
+				throw new Exception("列數必須小於10");
+			}
+			return row;
+		}
+
+		private void btmMiddle_Click(object sender, EventArgs e)
+		{
+			//取得列數
+			int row = 0;
+			row = GetRows();
+			//生成等腰星號
+			string stars = GenerateMiddleStars(row);
+			//呈現星號
+			Display(stars);
+		}
+
+		private void btmRight_Click(object sender, EventArgs e)
+		{    
+			//取得列數
+			int row = 0;
+			row = GetRows();
+			//生成靠右星號
+			string stars = GenerateRightStars(row);
+			//呈現星號
+			Display(stars);
+		}
+
+		private string GenerateRightStars(int row)
+		{
+			string stars = string.Empty;
+			string space = string.Empty;
+			for (int i = 1; i <= row; i++)
+			{
+				stars += new string('*', i).PadLeft(2*row-i)+"\r\n"; 
+			}
+			return stars;
+		}
+		private string GenerateMiddleStars(int row)
+		{
+			string stars = string.Empty;
+			string space = string.Empty;
+			for (int i = 1; i <= row; i++)
+			{
+				stars += new string('*', i).PadLeft(row) + "\r\n";
+			}
+			return stars;
+		}
+	}
+}
